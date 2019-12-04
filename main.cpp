85020d71475c0aa1b264b3bab505600120bf6891
thhyj <276897571@qq.com>
2019-12-04 17:49:14 +0800

    the first try

---
+++ main.cpp
@@ -0,0 +1,10 @@
+#include <opencv2/opencv.hpp>
+#include <iostream>
+#include "opentheimg.cpp"
+using namespace std;
+using namespace cv;
+int main() {
+    string name = "bg-2";
+    opentheimg(name);
+    return 0;
+}
